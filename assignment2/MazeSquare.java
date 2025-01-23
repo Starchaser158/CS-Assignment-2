4461e64c9439ff30a2062ca33abd87f5b0232487
Starchaser158 <48892848+Starchaser158@users.noreply.github.com>
2025-01-22 23:40:20 -0600

    more buh

--- assignment2/MazeSquare.java
+++ assignment2/MazeSquare.java
@@ -7,18 +7,50 @@
  */
 public class MazeSquare {
     // Instance variables of your choosing
+    private int row;
+    private int col;
+    private int lilGuy;
+    private int squadGoals;
     
     // A constructor, taking whatever parameters you decide:
     public MazeSquare(int row, int col, String squareType) {
         // YOUR CODE HERE
+        this.row = row;
+        this.col = col;
+        lilGuy = squareType;
     }
 
     public MazeSquare(int row, int col, String squareType, String staFinish){
-
+        this.row = row;
+        this.col = col;
+        lilGuy = squareType;
+        squadGoals = staFinish
     }
+
+    public int getRow(){
+        return row;
+    }
+
+    public int getCol(){
+        return col;
+    }
+
+    public String getSquareType(){
+        return lilGuy;
+    }
+
     // Whatever methods you want, such as:
     public boolean hasTopWall() {
         // YOUR CODE (and new return statement) HERE
-        return false;
+        if(getRow() == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean hasSideWall(){
+        if()
     }
 }
